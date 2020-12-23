43e499fa29cc30c06ea79d9117136611320d5d00
cromwell421 <32655686+cromwell421@users.noreply.github.com>
2020-12-23 13:14:10 -0500

    Add files via upload
    r script for bot

---
+++ script.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+
+# unlink(Sys.getenv("TWITTER_PAT"))
+# Sys.unsetenv("TWITTER_PAT")
+
+# Create a token containing your Twitter keys
+token <-rtweet::create_token(
+  app = "atlanta_falcons_bot",  # the name of the Twitter app
+  consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+#get list of friends
+f <- rtweet::get_friends("@BotAtlanta")
+
+#make list of those friends
+accounts <- c(f$user_id)
+
+#Pull timeline all of all friends and filter to those whithin the last 4 hours
+tweets <- rtweet::get_timeline(accounts, n= 500, exclude_replies = TRUE) %>% 
+  filter(libridate::difftime(libridate::now(tzone = 'UTC'), created_at) < 240)
+
+falcons <- c('Falcons|Atlanta|ATL')
+#filter tweets to just falcons related
+falcons_post <- rtweet::tweets %>% 
+  filter(str_detect(text, falcons))
+
+#post_tweet using retweet_id equalt to the status id
+rtweet::post_tweet(retweet_id = falcons_post$status_id)
+
